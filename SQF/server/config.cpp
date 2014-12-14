76b71b6035e5ad859f8aa3f0760f8c86356e73eb
R4Z0R49 <R4Z0R49@gmail.com>
2014-12-14 14:51:05 +0000

    Basic Hive setup

---
+++ SQF/server/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+	class server {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {""};
+	};
+};
+
+class CfgFunctions 
+{
+	class s2017 
+	{
+
+	};
+};
